f0adc3314f942a8a1fc2d126a7fcb4cba48503ed
vikaspatil2277 <vikaspatil2277@gmail.com>
2023-04-04 19:45:46 +0530

    modified

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ import Navbar from './Navbar'
 
 const Contact = () => {
   return (
-    <div>
+    <div className=''>
         <Navbar/>
         <h1 style={{textAlign:"center"}}>Contact US:- +91 98765432109</h1>
         <h1 style={{textAlign:"center"}}>Email:-something@gmail.com</h1>
