5d78175a00da593f3368683d243b02175ca3658a
vikaspatil2277 <vikaspatil2277@gmail.com>
2023-04-05 18:09:27 +0530

    mofified

--- src/components/Students.js
+++ src/components/Students.js
@@ -1,14 +1,83 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { Link } from 'react-router-dom'
+
+import '../App.css'
 
 const Students = () => {
   return (
     
-    <div>
-<Navbar/>
-        Students
-
-    </div>
+    (
+      <div>
+          <Navbar/>
+          <div className='std-title'>
+              <span className='stud'>Students Details</span>
+              <button className='add-student'>
+                  Add New Student
+              </button>
+          </div>
+          <div>
+              <table>
+                  <thead>
+                      <th>Name</th>
+                      <th>Age</th>
+                      <th>Course</th>
+                      <th>Batch</th>
+                      <th>Changes</th>
+                  </thead>
+                  <tr>
+                      <td>vikas Patil</td>
+                      <td>24</td>
+                      <td>MERN</td>
+                      <td>October</td>
+                      <td><Link to={'/edit'}>Edit</Link> </td>
+                  </tr>
+                  <tr>
+                      <td>vikas Patil</td>
+                      <td>24</td>
+                      <td>MERN</td>
+                      <td>October</td>
+                      <td><Link to={'/edit'}>Edit</Link> </td>
+                  </tr>
+                  <tr>
+                      <td>vikas Patil</td>
+                      <td>24</td>
+                      <td>MERN</td>
+                      <td>October</td>
+                      <td><Link to={'/edit'}>Edit</Link> </td>
+                  </tr>
+                  <tr>
+                      <td>vikas Patil</td>
+                      <td>24</td>
+                      <td>MERN</td>
+                      <td>October</td>
+                      <td><Link to={'/edit'}>Edit</Link> </td>
+                  </tr>
+                  <tr>
+                      <td>vikas Patil</td>
+                      <td>24</td>
+                      <td>MERN</td>
+                      <td>October</td>
+                      <td><Link to={'/edit'}>Edit</Link> </td>
+                  </tr>
+                  <tr>
+                      <td>vikas Patil</td>
+                      <td>24</td>
+                      <td>MERN</td>
+                      <td>October</td>
+                      <td><Link to={'/edit'}>Edit</Link> </td>
+                  </tr>
+                  <tr>
+                      <td>vikas Patil</td>
+                      <td>24</td>
+                      <td>MERN</td>
+                      <td>October</td>
+                      <td><Link to={'/edit'}>Edit</Link> </td>
+                  </tr>
+              </table>
+          </div>
+      </div>
+    )
   )
 }
 
